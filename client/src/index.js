cd922341bfedd0563e419ad89ec1f6f3e9e6a8d9
unknown <kaden.zipfel@hotmail.com>
2018-12-05 16:35:38 -0800

    Setup redux form

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import {BrowserRouter, Route} from 'react-router-dom';
-import Welcome from './components/Welcome';
-import Signup from './components/auth/Signup';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+
+import App from './components/App';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 import reducers from './reducers';
 
 ReactDOM.render(
